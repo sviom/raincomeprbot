956f96c561d795a394a21ae6f66dc64d4f6be912
hanbyulkang <hanbyulkang@jinhak.com>
2021-12-17 10:36:01 +0900

    키볼트용 환경변수 추가 시작

--- bot/Helper/AzureKeyVaultHelper.js
+++ bot/Helper/AzureKeyVaultHelper.js
@@ -1,7 +1,11 @@
 const { DefaultAzureCredential } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
+const dotenv = require("dotenv");
+
 const vaultName = "todaylunchkeyvault";
 const url = `https://${vaultName}.vault.azure.net`;
+dotenv.config({ path: '/bot/azure.env' });
+
 
 async function GetKeyVaultSecret(secretName = 'RaincomePrConnectionString') {
     const credential = new DefaultAzureCredential();
@@ -10,6 +14,39 @@ async function GetKeyVaultSecret(secretName = 'RaincomePrConnectionString') {
     console.log(`Latest version of the secret ${secretName}: `, latestSecret);
     // const specificSecret = await client.getSecret(secretName, { version: latestSecret.properties.version! });
     // console.log(`The secret ${secretName} at the version ${latestSecret.properties.version!}: `, specificSecret);
+}
+
+
+async function main() {
+    // DefaultAzureCredential expects the following three environment variables:
+    // - AZURE_TENANT_ID: The tenant ID in Azure Active Directory
+    // - AZURE_CLIENT_ID: The application (client) ID registered in the AAD tenant
+    // - AZURE_CLIENT_SECRET: The client secret for the registered application
+    const credential = new DefaultAzureCredential();
+
+    const url = process.env["KEYVAULT_URI"] || "<keyvault-url>";
+
+    const client = new SecretClient(url, credential);
+
+    // Create a secret
+    const uniqueString = new Date().getTime();
+    const secretName = `secret${uniqueString}`;
+    const result = await client.setSecret(secretName, "MySecretValue");
+    console.log("result: ", result);
+
+    // Read the secret we created
+    const secret = await client.getSecret(secretName);
+    console.log("secret: ", secret);
+
+    // Update the secret with different attributes
+    const updatedSecret = await client.updateSecretProperties(secretName, result.properties.version, {
+        enabled: false
+    });
+    console.log("updated secret: ", updatedSecret);
+
+    // Delete the secret
+    // If we don't want to purge the secret later, we don't need to wait until this finishes
+    await client.beginDeleteSecret(secretName);
 }
 
 export {
